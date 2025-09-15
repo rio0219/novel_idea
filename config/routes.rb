dd35742a5ed9acd7efaa70915ca4b34b52f279d4
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-09-15 22:29:04 +0900

    Rubocop 指摘対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # root を pages#home に変更
   root "pages#home"
-  
+
   get "home/index"
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
