2e9482c41fc56dca7918b2ac663b59ff8d68ca8e
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-09-22 00:01:43 +0900

    Devise のログインを組み込みテスト実行

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers  # Devise のテストヘルパーを追加
+
+  def setup
+    @user = users(:one)  # fixtures のユーザーを読み込む
+    sign_in @user        # ログイン状態にする
+  end
+
   test "should get index" do
-    get home_index_url
+    get home_index_path
     assert_response :success
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers  # Devise のテストヘルパーを追加
+
+  def setup
+    @user = users(:one)  # fixtures のユーザーを読み込む
+    sign_in @user        # ログイン状態にする
+  end
+
   test "should get home" do
-    get root_url
+    get root_path
     assert_response :success
   end
 end
