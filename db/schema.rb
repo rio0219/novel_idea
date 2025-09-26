9f2ecbe5df6073b1db1ca3a082f3a37295ef5b0e
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-09-26 22:58:27 +0900

    post関連CRUD + 画面作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_23_130850) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_26_122024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_23_130850) do
     t.index ["work_id"], name: "index_characters_on_work_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -44,6 +54,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_23_130850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["work_id"], name: "index_plots_on_work_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "genre_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_posts_on_genre_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,7 +102,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_23_130850) do
   end
 
   add_foreign_key "characters", "works"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "plots", "works"
+  add_foreign_key "posts", "genres"
+  add_foreign_key "posts", "users"
   add_foreign_key "works", "genres"
   add_foreign_key "works", "users"
   add_foreign_key "worldviews", "works"
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,13 +1,6 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-
-  def setup
-    @user = users(:user_one)
-    sign_in @user
-  end
-
   test "should get home" do
     get root_path
     assert_response :success
