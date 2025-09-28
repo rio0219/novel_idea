e7629a1c17c7b0e8a5327542470105f9a518befd
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-09-29 01:08:56 +0900

    マイページ&postサイドバー実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  layout "posts"
+  before_action :set_user, only: [ :show, :edit, :update ]
+
+  def show
+    @posts = @user.posts
+  end
+
+  def edit
+    redirect_to @user unless @user == current_user
+  end
+
+  def update
+    if @user == current_user && @user.update(user_params)
+      redirect_to @user, notice: "プロフィールを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :bio)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 名前が未設定の場合のデフォルト
+  def display_name
+    name.present? ? name : "名無しさん"
+  end
+
+  # イニシャルアイコン用
+  def initial_icon
+    display_name[0].upcase
+  end
 end
