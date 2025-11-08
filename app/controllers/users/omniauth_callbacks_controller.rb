4cdd8e2476118aee5895b6f2d8142a9d746af643
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-11-08 10:35:07 +0900

    LINEログイン機能の実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,5 +11,17 @@ module Users
         redirect_to new_user_registration_url, alert: "Googleアカウントでの認証に失敗しました。"
       end
     end
+
+    def line
+      user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if user.persisted?
+        sign_in_and_redirect user, event: :authentication
+        set_flash_message(:notice, :success, kind: "LINE") if is_navigational_format?
+      else
+        session["devise.line_data"] = request.env["omniauth.auth"].except(:extra)
+        redirect_to new_user_registration_url, alert: "LINEアカウントでの認証に失敗しました。"
+      end
+    end
   end
 end
