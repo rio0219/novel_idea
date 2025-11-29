6a735360b5fc68fd6bda0e9c56ec8c34dcee85d1
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-11-30 07:19:15 +0900

    Add Gmail SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,16 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'www.tsumugiba-novel.com', protocol: 'https' }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               ENV["SMTP_DOMAIN"],
+    user_name:            ENV["SMTP_USER"],
+    password:             ENV["SMTP_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
