0d7f38678ac631d19ed96538e749d7e8cf9da210
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-11-30 17:11:37 +0900

    js修正

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,3 +1,2 @@
 // Import all the channels to be used by Action Cable
-const channels = require.context(".", true, /_channel\.js$/);
-channels.keys().forEach(channels);
+import "channels/comment_channel"
