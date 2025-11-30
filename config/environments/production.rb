1db7fd4b531ebfd17bfa251ad70a46d60d3101d2
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-11-30 14:28:06 +0900

    fix session cookie samesite for google oauth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   }
 
   config.action_controller.default_url_options = { host: 'www.tsumugiba-novel.com', protocol: 'https' }
+  config.action_dispatch.cookies_same_site_protection = :none
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
