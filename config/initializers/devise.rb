eff57949d8ac26ab89770aebf81c9b9f7fd16327
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-11-30 13:11:04 +0900

    redirect_uri削除

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -301,8 +301,7 @@ Devise.setup do |config|
     {
       scope: "email,profile",
       prompt: "select_account",
-      skip_jwt: true,
-      redirect_uri: "https://www.tsumugiba-novel.com/users/auth/google_oauth2/callback"
+      skip_jwt: true
     }
 
   config.omniauth :line,
