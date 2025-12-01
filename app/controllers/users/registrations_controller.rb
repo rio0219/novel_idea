aca751134d09b06172b1939a0e62b617557ac9f6
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-12-02 02:04:03 +0900

    Turbo無効化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,17 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    before_action :skip_turbo
     def create
       super do |user|
         sign_in(user) if user.persisted? # 登録完了後に自動ログイン
       end
+    end
+
+
+    private
+
+    def skip_turbo
+      request.headers["Turbo-Frame"] = nil
     end
   end
 end
